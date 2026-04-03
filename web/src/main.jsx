d5ed1cb67102236d8f4a7084ffa346e4c6f3a92e
Rahul Gupta <aicountly@gmail.com>
2026-04-03 12:42:08 +0530

    fix: wrap app in MsalProvider for MSAL v5 compatibility

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { MsalProvider } from '@azure/msal-react'
+import { msalInstance } from './auth/MsalConfig.js'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
   </StrictMode>,
 )
