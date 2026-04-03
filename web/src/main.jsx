5fcf557b8c5cc7d1d503e01f41b834d2465bbf3b
Rahul Gupta <aicountly@gmail.com>
2026-04-03 14:00:45 +0530

    fix: process MSAL redirect response and save session on return

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,11 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { MsalProvider } from '@azure/msal-react'
 import { msalInstance } from './auth/MsalConfig.js'
+import { loginWithMicrosoft } from './services/authService.js'
 import './index.css'
 import App from './App.jsx'
 
 msalInstance.initialize().then(() => {
-  msalInstance.handleRedirectPromise().catch(() => {}).finally(() => {
+  msalInstance.handleRedirectPromise().then((response) => {
+    if (response && response.idToken) {
+      loginWithMicrosoft(response).catch(() => {})
+    }
+  }).catch(() => {}).finally(() => {
     createRoot(document.getElementById('root')).render(
       <StrictMode>
         <MsalProvider instance={msalInstance}>
